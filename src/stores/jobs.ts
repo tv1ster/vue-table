5f6ee35584531549ea313ef9654d5ac18fbb6cb1
vadym <vadym.ruchka@gmail.com>
2024-10-25 15:13:59 +0200

    Implement mapping jobs to time tables

--- src/stores/jobs.ts
+++ src/stores/jobs.ts
@@ -1,10 +1,34 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import { type AvailableMachine, isValidData, type Job, type MachineTimeTable, TaskName } from './types'
+import {
+  type AvailableMachine,
+  isValidData,
+  type Job,
+  type MachineTimeTable,
+  TaskName
+} from './types'
+
+const availableMachines: AvailableMachine[] = [
+  {
+    type: TaskName.Print,
+    startTime: 8,
+    duration: 14,
+  },
+  {
+    type: TaskName.Laminate,
+    startTime: 8,
+    duration: 14,
+  },
+  {
+    type: TaskName.Trim,
+    startTime: 8,
+    duration: 14,
+  },
+];
 
 export const useJobsStore = defineStore('counter', () => {
-  const loading = ref(true)
-  const jobs = ref<readonly Job[]>([])
+  const loading = ref(true);
+  const timeTables = ref<readonly MachineTimeTable[]>([]);
 
   fetch('src/data/data.json').then(
     data => {
@@ -15,9 +39,9 @@ export const useJobsStore = defineStore('counter', () => {
       if (!isValidData(data)) {
         throw new Error('Invalid data format')
       }
-      jobs.value = data.jobs;
-      console.log('Fetched data:', data.jobs)
-      loading.value = false
+      loading.value = false;
+      timeTables.value = fillMachinesTimeTables(availableMachines, data.jobs);
+      console.log('timetables:', fillMachinesTimeTables(availableMachines, data.jobs))
     }
   ).catch(
     error => {
@@ -25,27 +49,67 @@ export const useJobsStore = defineStore('counter', () => {
     }
   )
 
-  return { loading, jobs }
+  return { loading, timeTables }
 })
 
-const availableMachines: AvailableMachine[] = [
-  {
-    type: TaskName.Print,
-    startTime: 9,
-    duration: 12,
-  },
-  {
-    type: TaskName.Laminate,
-    startTime: 9,
-    duration: 12,
-  },
-  {
-    type: TaskName.Trim,
-    startTime: 9,
-    duration: 12,
-  },
-];
+export function fillMachinesTimeTables(availableMachines: readonly AvailableMachine[], jobs: readonly Job[]): readonly MachineTimeTable[] {
+  const emptyMachines: MachineTimeTable[] = availableMachines.map(machine => ({
+    ...machine,
+    occupiedSlots: [],
+    availableSlots: [{ startTime: machine.startTime, duration: machine.duration }],
+  }));
 
-export function fillMachinesTimeTables(machines: readonly AvailableMachine[], jobs: readonly Job[]): readonly MachineTimeTable[] {
-  return [];
+  return jobs.reduce((machines, job) => {
+    let prevTaskEndTime = 0;
+    const newMachines: MachineTimeTable[] = [...machines];
+    job.tasks.forEach(task => {
+      let changed = false;
+      const machine = machines.find(machine => machine.type === task.taskName);
+      if (!machine) {
+        return;
+      }
+      machine.availableSlots.forEach((slot, i) => {
+        if (changed) {
+          return;
+        }
+        const diff = prevTaskEndTime > slot.startTime ? prevTaskEndTime - slot.startTime : 0;
+        // TODO: handling for scenarios, when there is no available slot
+        if (slot.duration - diff >= task.taskDuration) {
+          const availableSlots = diff === 0
+            ? [{ startTime: slot.startTime + task.taskDuration, duration: slot.duration - task.taskDuration }]
+            : task.taskDuration + diff === slot.duration
+              ? [{ startTime: slot.startTime, duration: diff }]
+              : [
+                { startTime: slot.startTime, duration: diff },
+                { startTime: slot.startTime + task.taskDuration + diff, duration: slot.duration - task.taskDuration - diff },
+              ];
+          const machineIndex = newMachines.findIndex(m => m.type === machine.type);
+          newMachines[machineIndex] = {
+            ...machine,
+            availableSlots: [
+              ...machine.availableSlots.slice(0, i),
+              ...availableSlots,
+              ...machine.availableSlots.slice(i + 1),
+            ].sort((a, b) => a.startTime - b.startTime),
+            occupiedSlots: [
+              ...machine.occupiedSlots,
+              {
+                startTime: slot.startTime + diff,
+                duration: task.taskDuration,
+                jobId: job.jobId,
+                taskId: task.taskId,
+              },
+            ],
+          };
+          prevTaskEndTime = slot.startTime + diff + task.taskDuration;
+          changed = true;
+        }
+      })
+    })
+    // fill in case job don't have tasks fot all machines
+    if (job.tasks.length < availableMachines.length) {
+
+    }
+    return newMachines;
+  }, emptyMachines);
 }
